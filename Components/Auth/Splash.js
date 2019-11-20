54a5e26a0abe1d9a7c1ced43a1239385a0ff24d3
unknown <gunjankalita836@gmail.com>
2019-11-20 04:26:17 -0600

    Added splash screen with progressbar

--- Components/Auth/Splash.js
+++ Components/Auth/Splash.js
@@ -1,34 +1,66 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text,ProgressBarAndroid, StyleSheet, View } from 'react-native'
+
 
 export default class Splash extends Component {
     render() {
         return (
             <View style={styles.container}>
-              <Text>EATS</Text>
-                <View >
-                  <Text style={styles.uber}>UBER</Text>
-                  <Text>EATS</Text>
+                <View style={styles.textLayout} >
+                  <Text style={styles.textWhite}>UBER</Text>
+                  <Text style={styles.textYellow}>EATS</Text>
+                </View>
+                <View style={styles.other}>
+                <ProgressBarAndroid styleAttr="Horizontal" indeterminate={true} progress={0.5} style={styles.progress} />                
                 </View>
             </View>
         )
     }
+
+    componentDidMount =()=>{
+        setTimeout(()=>{
+            this.props.navigation.replace("Home"); 
+        }, 2000); 
+    }
+
 }
 
 const styles = StyleSheet.create({
     container:{
         backgroundColor: "#000", 
-        flex: 1
+        flex: 1, 
+        flexDirection:"column",
     }, 
-    logo:{
-        alignItems:"center", 
-        justifyContent: "center"
+    textLayout:{
+        height:"99%",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    other:{
+        height:"1%",
+        backgroundColor:"#000",
+    },
+    textWhite:{
+        color: "#fff", 
+        fontWeight:"900",
+        fontSize:40
     }, 
+    textYellow:{
+        color: "#ff0", 
+        fontWeight:"900",
+        fontSize:40
+    }, 
+    progress:{
+        height:"100%",
+        justifyContent:"space-around",
+        padding:0,
+        color:"#0f0"
+    },
     uber:{
         color: "#fff", 
         flex: 1,
-        fontSize: 20,
-        textAlign:"center", 
-        textAlignVertical:"center"
+        fontSize: 20, 
+        justifyContent: "center",
+        alignItems:"center"
     }
 })
